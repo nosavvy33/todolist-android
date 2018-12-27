5f66c147c78bd1ffefd65fa1c1916b33caec6202
nosavvy33 <bleonp1997@gmail.com>
2018-12-27 10:31:36 -0500

    Text and Color Values refactored

--- app/src/main/java/com/example/undead/todolist/MainActivity.java
+++ app/src/main/java/com/example/undead/todolist/MainActivity.java
@@ -50,12 +50,7 @@ public class MainActivity extends AppCompatActivity implements ProductsAdapter.P
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
 
